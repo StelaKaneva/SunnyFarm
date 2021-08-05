0d13f56dae1acabe0109a4a3cfa0b10d28156196
Stela Kaneva <stela.marinova.eneva@gmail.com>
2021-08-05 13:06:51 +0300

    Update Home Page

--- SunnyFarm/Controllers/HomeController.cs
+++ SunnyFarm/Controllers/HomeController.cs
@@ -7,7 +7,7 @@
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
-        
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
