b4482ce372ba1f59367bfbd03b8a4ca480fed7a6
Stela Kaneva <stela.marinova.eneva@gmail.com>
2021-08-15 15:39:41 +0300

    Implement Web API for Products

---
+++ SunnyFarm/Controllers/Api/ProductsApiController.cs
@@ -0,0 +1,39 @@
+﻿namespace SunnyFarm.Controllers.Api
+{
+    using System.Collections;
+    using System.Linq;
+    using Microsoft.AspNetCore.Mvc;
+    using SunnyFarm.Data;
+
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsApiController : ControllerBase
+    {
+        private readonly SunnyFarmDbContext data;
+
+        public ProductsApiController(SunnyFarmDbContext data)
+        {
+            this.data = data;
+        }
+
+        [HttpGet]
+        public IEnumerable GetProducts()
+        {
+            return this.data.Products.ToList();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetDetails(int id)
+        {
+            var product = this.data.Products.Find(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+    }
+}
